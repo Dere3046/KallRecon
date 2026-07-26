7a38279e7428c600d6b041a13e185e7c1a5ecfe3
deref3046 <fine4trn@163.com>
2026-07-26 23:09:29 +0800

    extend ti scan across page boundary

--- lib/core.c
+++ lib/core.c
@@ -464,8 +464,16 @@ static unsigned long find_token_index(unsigned long start)
 			unsigned int *buf = &bigbuf[pi * 1024];
 			unsigned long base = pg + pi * 0x1000;
 
-			for (int off = 512; off < 0x1000; off += 4) {
-				unsigned short *ti = (unsigned short *)((unsigned char *)buf + off - 512);
+			for (int off = 512; off < 0x1000 + 512; off += 4) {
+				unsigned short *ti;
+				if (off < 0x1000) {
+					ti = (unsigned short *)((unsigned char *)buf + off - 512);
+				} else {
+					if (pi == 15)
+						break;
+					ti = (unsigned short *)((unsigned char *)
+						&bigbuf[(pi + 1) * 1024] + off - 0x1000 - 512);
+				}
 				if (!check_ti_strong(ti))
 					continue;
 				return base + off - 512;
